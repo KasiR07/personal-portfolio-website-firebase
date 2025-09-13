f30c08031df60f15fdf30226dadc34298b1befe4
N. V. Sai Ruthvik Kasi <knvsruthvik07@gmail.com>
2025-09-13 07:52:29 +0000

    More formal please

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,13 +6,10 @@ import Image from 'next/image';
 const Hero = () => {
   return (
     <section id="home" className="relative container flex flex-col md:flex-row items-center justify-between min-h-[calc(100vh-3.5rem)] py-20">
-       <div className="absolute inset-0 -z-10 h-full w-full bg-background">
-        <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-        <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
-      </div>
+       <div className="absolute inset-0 -z-10 h-full w-full bg-background" />
       <div className="md:w-1/2 text-center md:text-left">
         <p className="text-lg font-headline text-primary sm:text-xl md:text-2xl mb-2">
-            Hi, I'm {personalData.name}
+            N. V. Sai Ruthvik Kasi
         </p>
         <h1 className="text-4xl font-headline font-bold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
             {personalData.role}
